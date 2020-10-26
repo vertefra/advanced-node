80d3244d0c3e3bfd6f5925b434c865d43f2342f8
vertefra <verte.fra@gmail.com>
2020-10-26 10:50:00 -0400

    config fix

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.DEV_DB,
   cookieKey: process.env.COOKIE_KEY,
-};
+}
